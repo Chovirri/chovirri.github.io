db7d600d78b14ee3d4868013c0a8a112ebdbe160
Chovirri <cdtjose123@gmail.com>
2024-06-30 14:23:41 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// Puedes agregar JavaScript para interacciones más complejas si lo deseas
+// Por ejemplo, para mostrar un mensaje cuando se haga clic en un enlace
+// o para cambiar el contenido de la página web de forma dinámica.
